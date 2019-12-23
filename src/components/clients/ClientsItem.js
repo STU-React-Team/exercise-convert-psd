9762e37ae86d052b244b43637c773f1380e66496
Tan Nguyen <tannguyen12.07.sk@gmail.com>
2019-12-23 13:45:17 +0700

    Finish Client - Logo

--- src/components/clients/ClientsItem.js
+++ src/components/clients/ClientsItem.js
@@ -2,23 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function ClientsItem(props) {
-    const {name, desc, img, textRight} = props
-    return <div className="clients__item">
-        <div className={textRight ? "clients__text" : "clients__text clients__text--right"}>
-            <p> {desc} </p>
-            <p> {name} </p>
-        </div>
-        <div className="clients__img">
-            <img src={img} alt={name}/>
-        </div>
+  const { name, desc, img, textRight } = props;
+  return (
+    <div className="clients__item">
+      <div
+        className={
+          textRight ? 'clients__text' : 'clients__text clients__text--right'
+        }>
+        <p> {desc} </p>
+        <p> {name} </p>
+      </div>
+      <div className="clients__img">
+        <img src={img} alt={name} />
+      </div>
     </div>
+  );
 }
 
 ClientsItem.propTypes = {
-    name: PropTypes.string.isRequired,
-    desc: PropTypes.string.isRequired,
-    img: PropTypes.string.isRequired,
-    textRight: PropTypes.bool.isRequired,
+  name: PropTypes.string.isRequired,
+  desc: PropTypes.string.isRequired,
+  img: PropTypes.string.isRequired,
+  textRight: PropTypes.bool.isRequired,
 };
 
 export default ClientsItem;
